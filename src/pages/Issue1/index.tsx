9a1786178ee01d187d746454335303bac07ad669
LSD <1977380266@qq.com>
2024-10-19 02:06:26 +0800

    feat: pkg

--- src/pages/Issue1/index.tsx
+++ src/pages/Issue1/index.tsx
@@ -1,16 +1,23 @@
 import { title, subtitle } from "@/components/primitives";
 import DefaultLayout from "@/layouts/default";
+import { Button } from "@nextui-org/button";
+import { useNavigate } from "react-router-dom";
 
 export default function Issue6() {
+  let navigate = useNavigate();
+
   return (
     <DefaultLayout>
       <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
         <div className="inline-block max-w-lg text-center justify-center">
           <span className={title()}>OUR&nbsp;</span>
-          <span className={title({ color: "violet" })}>CITY&nbsp;</span>
+          <span className={title({ color: "blue" })}>CITY&nbsp;</span>
           <br />
-          <span className={title()}>第一期</span>
+          <span className={title()}>第1期</span>
           <div className={subtitle({ class: "mt-4" })}>文件整理中</div>
+          <Button color="primary" className="mt-20" onClick={() => navigate("/our-city/issue6")} size="lg">
+            首页
+          </Button>
         </div>
       </section>
     </DefaultLayout>

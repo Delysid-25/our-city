caa80b5d7ce6bfd05d5007563d1eda2952df6268
LSD <1977380266@qq.com>
2024-10-19 01:28:55 +0800

    feat: pkg

--- src/config/site.ts
+++ src/config/site.ts
@@ -14,27 +14,27 @@ export const siteConfig = {
   navMenuItems: [
     {
       label: "第一期",
-      href: `${base}/issue6`,
+      href: `${base}/issue1`,
     },
     {
       label: "第二期",
-      href: `${base}/issue5`,
-    },
-    {
-      label: "第三期",
-      href: `${base}/issue4`,
-    },
-    {
-      label: "第四期",
-      href: `${base}/issue3`,
-    },
-    {
-      label: "第五期",
       href: `${base}/issue2`,
     },
     {
+      label: "第三期",
+      href: `${base}/issue3`,
+    },
+    {
+      label: "第四期",
+      href: `${base}/issue4`,
+    },
+    {
+      label: "第五期",
+      href: `${base}/issue5`,
+    },
+    {
       label: "第六期",
-      href: `${base}/issue1`,
+      href: `${base}/issue6`,
     },
   ],
 };

491c8c02559e2a3527489fc53f168eed4c70867a
LSD <1977380266@qq.com>
2024-10-19 00:55:16 +0800

    feat: pkg

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,17 @@ import Issue2 from "./pages/Issue2";
 import Issue1 from "./pages/Issue1";
 
 import Issue6 from "@/pages/Issue6";
-
+let base = "/our-city";
 function App() {
   return (
     <Routes>
-      <Route element={<Issue6 />} path="/issue6" />
-      <Route element={<Issue5 />} path="/issue5" />
-      <Route element={<Issue4 />} path="/issue4" />
-      <Route element={<Issue3 />} path="/issue3" />
-      <Route element={<Issue2 />} path="/issue2" />
-      <Route element={<Issue1 />} path="/issue1" />
-      <Route element={<Navigate replace to="/issue6" />} path="/" />
+      <Route element={<Issue6 />} path={`${base}/issue6`} />
+      <Route element={<Issue5 />} path={`${base}/issue5`} />
+      <Route element={<Issue4 />} path={`${base}/issue4`} />
+      <Route element={<Issue3 />} path={`${base}/issue3`} />
+      <Route element={<Issue2 />} path={`${base}/issue2`} />
+      <Route element={<Issue1 />} path={`${base}/issue1`} />
+      <Route element={<Navigate replace to={`${base}/issue6`} />} path="/" />
     </Routes>
   );
 }
